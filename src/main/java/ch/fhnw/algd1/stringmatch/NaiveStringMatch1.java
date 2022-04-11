d83974b8b851d18e09b08f5f64bd580c436c2413
Wolfgang Weck <wolfgang.weck@surface7>
2022-04-11 16:29:51 +0200

    swaps methods `matches` and `firstMatch`in template

--- src/main/java/ch/fhnw/algd1/stringmatch/NaiveStringMatch1.java
+++ src/main/java/ch/fhnw/algd1/stringmatch/NaiveStringMatch1.java
@@ -3,14 +3,15 @@ package ch.fhnw.algd1.stringmatch;
 import ch.fhnw.algd1.stringmatch.framework.IStringMatch;
 
 public class NaiveStringMatch1 implements IStringMatch {
-	@Override
-	public int firstMatch(String text, String pattern) {
-		// TODO (A2) sequential search for first i such that matches(text, i, pattern)
-		return 0;
-	}
-
 	private boolean matches(String text, int start, String pattern) {
 		// TODO (A2) return if pattern matches text from position start
 		return false;
+	}
+
+	@Override
+	public int firstMatch(String text, String pattern) {
+		// TODO (A2) sequential search for first i such that matches(text, i,
+		// pattern)
+		return 0;
 	}
 }
